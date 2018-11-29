e1b473d148bfccc4e6222a3d770b9eecfe0c32ab
Tonyjlum <tonyjlum@gmail.com>
2018-11-29 10:23:10 -0500

    SQLIT

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 35, "M", "red", "optimistic", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Rowdy", 20, "F", "green", "social", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Tabitha", 2, "F", "blue", "goofy", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Sergeant Brown", 26, "F", "brownish", "short-tempered", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Melissa", 12, "F", "brown", "melancholic", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Grinch", 9, "M", "green", "quiet", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Wendy", 9, "F", "orange", "analytical", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, NULL, NULL, NULL, NULL, "friendly", 1);
