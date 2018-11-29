d051695395014414bad84655c2bfa89efb2da2e4
isaac <oseasisaac91@gmail.com>
2018-11-29 09:41:10 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 10, 'Female', 'white', 'normal', 1);
+INSERT INTO bears VALUES (2, "Rowdy", 10, 'Female', 'brown', 'normal', 1);
+INSERT INTO bears VALUES (3, "Tabitha", 10, 'Male', 'white', 'normal', 0);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 10, 'Female', 'brown', 'normal', 1);
+INSERT INTO bears VALUES (5, "Melissa", 10, 'Male', 'white', 'normal', 1);
+INSERT INTO bears VALUES (6, "Grinch", 10, 'Male', 'brown', 'normal', 0);
+INSERT INTO bears VALUES (7, "Wendy", 10, 'Female', 'white', 'normal', 1);
+INSERT INTO bears VALUES (8, NULL, NULL, 'Female', 'brown', 'normal', 1);
+--id INTEGER, name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, alive BOOLEAN
