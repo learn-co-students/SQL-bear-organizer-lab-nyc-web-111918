29ae76cda5509228eb55c779e69277f41bce2eb3
dhamilt9 <danhamilton16@gmail.com>
2018-11-29 10:51:47 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "Male", "Black", "Angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, "Male", "Black", "Angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 1, "Male", "Black", "Angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 1, "Male", "Black", "Angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 1, "Male", "Black", "Angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 1, "Male", "Black", "Angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 1, "Male", "Black", "Angry", 1);
+INSERT INTO bears (age, gender, color, temperament, alive) VALUES (1, "Male", "Black", "Angry", 1);
+
+
+  -- id INTEGER PRIMARY KEY,
+  -- name TEXT,
+  -- age INTEGER,
+  -- gender TEXT,
+  -- color TEXT,
+  -- temperament TEXT,
+  -- alive INTEGER
