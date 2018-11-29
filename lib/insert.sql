e28555e7062eb63a261315f790162a038323b0f4
Rakugen <simonlee0306@gmail.com>
2018-11-29 11:06:59 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,25 @@
+/*   id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  gender TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+*/
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1, 'Mr. Chocolate', 17, 'Male', 'brown', 'sweet', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(2, 'Rowdy', 8, 'Male', 'black', 'angro', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(3, 'Tabitha', 5, 'Female', 'black', 'edgy', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(4, 'Sergeant Brown', 11, 'Male', 'brown', 'strict', 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(5, 'Melissa', 2, 'Female', 'white', 'calm', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(6, 'Grinch', 5, 'Male', 'green', 'deceitful', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(7, 'Wendy', 8, 'Female', 'white', 'feisty', 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(8, NULL, 14, 'Male', 'brown', 'deadly', 1);
